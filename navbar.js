99815a2db04e881889dfabc92964b9155b55010e
Holly Smith <hollyschuldt@gmail.com>
2023-11-09 00:20:24 -0600

    added navbar js with reusable function. amended oviewview to be specific top and bottom domstrings. everyone can now render their top and bottom divs with topDomString and bottomDomString

---
+++ navbar.js
@@ -0,0 +1,26 @@
+const overviewLink = document.querySelector('#overview');
+const repositoriesLink = document.querySelector('#Repositories');
+const projectsLink = document.querySelector('#projects');
+const packagesLink = document.querySelector('#packages');
+const mainLink = document.querySelector('.active');
+
+
+const topBox = document.getElementById('topBox');
+const bottomBox = document.getElementById('bottomBox');
+
+
+overviewLink.addEventListener('click', (e) => {
+
+  e.preventDefault()
+
+  topBox.innerHTML = topDomString;
+
+  bottomBox.innerHTML = bottomDomString;
+
+
+  repositoriesLink.classList.remove('active');
+  packagesLink.classList.remove('active');
+  projectsLink.classList.remove('active');
+  overviewLink.classList.add('active');
+
+})
--- overview.js
+++ overview.js
@@ -1,26 +1,60 @@
-TODO: // create form to add repository cards to page - bottomBox
-// Form:  name, description, bottom row of type/star/forks/updated last
-`<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+
+const bottomDomString = `    <div>
+<div class="container mt-5">
+  <div class="bg-dark text-light p-5">
+    <h2>Create a Pinned Repo</h2>
+    <p class="text-secondary">
+      Coordinate, track, and update your work in one place, so projects stay
+      transparent and on schedule.
+    </p>
+    <hr class="text-secondary" />
+    <form>
+      <div class="mb-3">
+        <label for="projectName" class="form-label">
+          Project board name
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="projectName"
+          placeholder="Example 2"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="projectDescription" class="form-label">
+          Description (optional)
+        </label>
+        <textarea
+          class="form-control"
+          id="projectDescription"
+          rows="3"
+          placeholder="Visit Github to see how they do this"
+        ></textarea>
+      </div>
+      <button type="submit" class="btn btn-success">
+        Create project
+      </button>
+    </form>
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+</div>
+</div>`
+
+const topDomString= `
+    <div class="container mt-5">
+      <div class="bg-dark text-light p-5">
+    <div class="repo-card">
+        <div class="repo-header">
+            <h3>Repository Name</h3>
+            <a href="https://github.com/yourusername/repo-name" class="repo-link">View on GitHub</a>
+        </div>
+        <p>Repository description goes here. This is a sample description of the pinned repository.</p>
+        <div class="repo-stats">
+            <span>type</span>
+            <span>Stars: 100</span>
+            <span>Forks: 50</span>
+        </div>
+    </div>
+      </div>
+    </div>
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>`
-
-TODO: // render pinned repo cards to page - topBox 
-
-
-
-
-TODO: // stretch - delete pins
-TODO: // stretch - about me square
+    `
