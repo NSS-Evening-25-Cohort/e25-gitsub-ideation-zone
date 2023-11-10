d8b7ab588a08455c9070a995014d524bcd662a2b
Holly Smith <hollyschuldt@gmail.com>
2023-11-09 19:46:57 -0600

    good on nav for now - going to work on overview box forms

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
-import { topDomString, bottomDomString } from "/overview.js";
-// import { bottomDomString } from "/overview.js";
+import { topDomString } from "/overview.js";
+import { bottomDomString } from "/overview.js";
 
 const overviewLink = document.querySelector('#overview');
 const repositoriesLink = document.querySelector('#Repositories');
@@ -26,5 +26,3 @@ overviewLink.addEventListener('click', (e) => {
   overviewLink.classList.add('active');
 
 })
-
-// think opposite - have all info on overview page - make this event listener an import function like renderToDom is
