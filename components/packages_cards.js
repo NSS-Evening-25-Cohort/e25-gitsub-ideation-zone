683795ceb014ee05be761b19d60809a9f2c3e832
Aaron Dugas <ajd3187@gmail.com>
2023-11-13 18:58:17 -0600

    Erica is helping me to push Packages page .js and .css

--- components/packages_cards.js
+++
@@ -1,5 +0,0 @@
-export const packagesCard =
-`<div class="Packages Card Container">
-        <h2>Title</h2>
-        <p>Description goes here. This is a sample description for the card element.</p>
-    </div>`;
